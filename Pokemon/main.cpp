9384efbddc50ac06d131f077bf167dc93d6555dd
Johnnie <ant.barron5925@gmail.com>
2025-07-02 14:15:15 -0500

    finish intro

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -4,8 +4,10 @@ using namespace std;
 string player_name = "";
 
 int main() {
-    cout << "Enter your name: ";
+    cout << "Trainer!Before you embark on your journey," << endl;
+    cout << "the Pokémon world needs to know who you are!" << endl;
+
     cin >> player_name;
-    cout << "Great start " << player_name << ", looks like you have understood the main() function properly now!";
+    cout << "Great start " << player_name << ", Looks like you’ve mastered the opening of every code adventure. Keep this up, and you’ll be a coding master in no time!";
     return 0;
 }
