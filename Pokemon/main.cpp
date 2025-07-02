da5599bc4bcb9523b7cc4cf442d764bfd8989f3a
Johnnie <ant.barron5925@gmail.com>
2025-07-02 16:00:54 -0500

    refactor if else to switch statement

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -11,27 +11,29 @@ int main() {
 
     cout << "Welcome to the world of pokemon, I am Professor Oak!" << endl;
     cout << "Before your jouney can begin select a pokemon!" << endl;
-    cout << "1. Bulbasour" << endl;
+    cout << "1. Bulbasaur" << endl;
     cout << "2. Squrtle" << endl;
     cout << "3. Charmander" << endl;
     cin >> pokemon_selection;
 
-    if (pokemon_selection == 1)
-    {
-        cout << "great choice, Bulbasour is the only right option!" << endl;
-    } else if (pokemon_selection == 2) {
-        cout << "squirtle is always a great option!" << endl;
-    } else if (pokemon_selection == 3) {
-        cout << "Charmander is a firey choice!" << endl;
-    } else {
-        cout << "Invalid input select 1-3" << endl;
+    switch (pokemon_selection) {
+         case 1:
+            cout << "great choice, Bulbasaur is the only right option!" << endl;
+            break;
+         case 2:
+            cout << "squirtle is always a great option!" << endl;
+            break;
+         case 3: 
+            cout << "Charmander is a firey choice!" << endl;
+            break;
+         default: 
+            cout << "Invalid input select 1-3" << endl;
     }
 
-    cout << "Ah, an excellent choice!" << endl;
     cout << "But beware, Trainer," << endl;
     cout << "this is only the beginning." << endl;
     cout << "Your journey is about to unfold." << endl;
-    cout << "Now let’s see if you’ve got what it takes to keep going!" << endl;
+    cout << "Now let's see if you've got what it takes to keep going!" << endl;
     cout << "Good luck, and remember… Choose wisely!" << endl;
         
     return 0;
